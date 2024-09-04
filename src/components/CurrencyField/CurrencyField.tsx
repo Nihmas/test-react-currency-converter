e34a1e3f282058c55a6a12e8b72eb51d85076646
Stan Samhin <stassamhin@gmail.com>
2024-09-04 21:34:09 +0200

    finish form

--- src/components/CurrencyField/CurrencyField.tsx
+++ src/components/CurrencyField/CurrencyField.tsx
@@ -1,12 +1,36 @@
+import {
+  SyntheticEvent
+} from "react";
+import {
+  Currency,
+  CurrencyCode
+} from "../../services/currencyApi.ts";
 import styles from './CurrencyField.module.css'
 
-export const CurrencyField = () => {
+type CurrencyFieldProps = {
+  list: Currency[];
+  value?: number;
+  onChange: (currencyCode: CurrencyCode, value: number) => void;
+  onCurrencyChange: (currencyCode: CurrencyCode) => void;
+  currencyCode: CurrencyCode | undefined;
+}
+
+export const CurrencyField = ({ list, onCurrencyChange, onChange, currencyCode, value }: CurrencyFieldProps) => {
+  const onSelect = (event: SyntheticEvent<HTMLSelectElement, Event>) => {
+    onCurrencyChange(event.target.value)
+  }
+
+  const onInputChange = (event: SyntheticEvent<HTMLInputElement, Event>) => {
+    if (currencyCode) {
+      onChange(currencyCode, event.target.value)
+    }
+  }
 
   return (
     <div className={styles.root}>
-      <input type="number" className={styles.input}/>
-      <select className={styles.select}>
-        <option>test</option>
+      <input type="number" className={styles.input} value={value} onChange={onInputChange} />
+      <select onChange={onSelect} className={styles.select} value={currencyCode}>
+        {list.map(({ short_code, name }) => (<option value={short_code} key={short_code}>{name}</option>))}
       </select>
     </div>
   )
--- src/components/CurrencyForm/CurrenciesForm.tsx
+++ src/components/CurrencyForm/CurrenciesForm.tsx
@@ -18,10 +18,48 @@ export const CurrencyForm = ({ currencies }: CurrenciesFormType) => {
   const [lastCurrency, setLastCurrency] = useState<CurrencyCode>("EUR");
   const [last, setLast] = useState<number | undefined>(undefined);
 
+  const onCurrencyChange = (type: 'first' | 'last') => (currencyCode: CurrencyCode) => {
+    const isFirst = type === 'first';
+
+    if (isFirst) {
+      setFirstCurrency(currencyCode)
+    } else {
+      setLastCurrency(currencyCode)
+    }
+  }
+
+  const onChange = async (currencyCode: CurrencyCode, amount: number) => {
+    if (!lastCurrency || !firstCurrency) {
+      return null;
+    }
+
+    const isFirst = firstCurrency === currencyCode;
+
+    if (isFirst) {
+      setFirst(amount);
+    } else {
+      setLast(amount);
+    }
+
+    console.log('currency code', currencyCode, amount);
+
+    const response = await currencyApi.compare(currencyCode, isFirst ? lastCurrency : firstCurrency, amount);
+
+    if (!response) {
+      return null;
+    }
+
+    if (isFirst) {
+      setLast(response.data.response.value);
+    } else {
+      setFirst(response.data.response.value);
+    }
+  }
+
   return (
     <div className={styles.root}>
-      <CurrencyField />
-      <CurrencyField />
+      <CurrencyField list={currencies} onCurrencyChange={onCurrencyChange('first')} onChange={onChange} currencyCode={firstCurrency} value={first} />
+      <CurrencyField list={currencies} onCurrencyChange={onCurrencyChange('last')} onChange={onChange} currencyCode={lastCurrency} value={last} />
     </div>
   )
 }
